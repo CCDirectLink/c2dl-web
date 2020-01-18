9efcf87f888d029b95b94a615c881371d3b920b6
streetclaw <streetclaw@c2dl.info>
2020-01-18 12:35:13 +0100

    fix style (darkmode+vars+restructure)

--- src/c2dl/resources/views/svgdata/chevron_right.blade.php
+++ src/c2dl/resources/views/svgdata/chevron_right.blade.php
@@ -1,7 +1,11 @@
 <svg width="100%" height="100%" viewBox="0 0 25 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <title>Chevron right</title>
-    <rect x="0" y="0" width="25" height="42" style="fill:none;"/>
-    <g id="Back" transform="matrix(1,3.69779e-32,1.2326e-32,1,-13.3,-376)">
-        <path d="M17.3,376L38.3,396.8L17.3,418L13.3,414L30.3,396.8L13.3,380L17.3,376Z" style="fill-rule:nonzero;"/>
+    <style>
+        .c2dl-lcv-bg{fill:{{ $bg ?? 'transparent' }};}
+        .c2dl-lcv-fg{fill:{{ $fg ?? 'rgb(25, 25, 25, 1.0)' }};}
+    </style>
+    <rect class="c2dl-lcv-bg" x="0" y="0" width="25" height="42"/>
+    <g transform="matrix(1,3.69779e-32,1.2326e-32,1,-13.3,-376)">
+        <path class="c2dl-lcv-fg" d="M17.3,376L38.3,396.8L17.3,418L13.3,414L30.3,396.8L13.3,380L17.3,376Z"/>
     </g>
 </svg>
