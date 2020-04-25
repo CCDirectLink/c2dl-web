97103d380d860ba42fcebc4b2b11a70b6830b726
streetclaw <streetclaw@c2dl.info>
2020-04-25 20:52:06 +0200

    fix: limit definition (name) - migrations

--- src/c2dl/database/migrations/2019_12_01_000000_create_acc_user_table.php
+++ src/c2dl/database/migrations/2019_12_01_000000_create_acc_user_table.php
@@ -15,7 +15,7 @@ class CreateAccUserTable extends Migration
     {
         Schema::connection('acc')->create('acc_user', function (Blueprint $table) {
             $table->bigIncrements('user_id')->comment('Unique user id');
-            $table->string('name')->unique()->comment('Unique user name');
+            $table->string('name', 64)->unique()->comment('Unique user name');
             $table->boolean('active')->default(false)
                 ->comment('True if user is usable/active - False user not exist (anymore)');
             $table->dateTime('validate_at')->nullable()
