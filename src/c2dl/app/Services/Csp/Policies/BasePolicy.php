97bec2d24020120800731e81926e8331b5695360
streetclaw <streetclaw@c2dl.info>
2021-02-19 12:50:15 +0100

    CSP added

---
+++ src/c2dl/app/Services/Csp/Policies/BasePolicy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\Csp\Policies;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Basic;
+use Spatie\Csp\Value;
+use Spatie\Csp\Keyword;
+
+class BasePolicy extends Basic
+{
+    public function configure()
+    {
+        $this
+            //->addDirective(Directive::UPGRADE_INSECURE_REQUESTS, Value::NO_VALUE)
+            //->addDirective(Directive::BLOCK_ALL_MIXED_CONTENT, Value::NO_VALUE)
+            ->addDirective(Directive::BASE, Keyword::SELF)
+            ->addDirective(Directive::CONNECT, Keyword::SELF)
+            ->addDirective(Directive::DEFAULT, Keyword::SELF)
+            ->addDirective(Directive::FORM_ACTION, Keyword::SELF)
+            ->addDirective(Directive::IMG, Keyword::SELF)
+            ->addDirective(Directive::MEDIA, Keyword::SELF)
+            ->addDirective(Directive::OBJECT, Keyword::NONE)
+            ->addDirective(Directive::SCRIPT, Keyword::SELF)
+            ->addDirective(Directive::STYLE, [Keyword::SELF, Keyword::UNSAFE_INLINE])
+            ->addNonceForDirective(Directive::SCRIPT);
+        // ->addNonceForDirective(Directive::STYLE)
+    }
+}
