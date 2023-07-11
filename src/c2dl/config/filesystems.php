2ed3a29f1448f6bd7017d6bde406f78dc801bc61
streetclaw <streetclaw@c2dl.info>
2023-07-11 22:12:25 +0200

    Fix filesystem

--- src/c2dl/config/filesystems.php
+++ src/c2dl/config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public/www-c2dl'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
