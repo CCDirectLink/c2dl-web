26e8938c1fd3f2d4fb6e55ed3a2482f20952207c
streetclaw <streetclaw@c2dl.info>
2024-04-27 10:09:50 +0200

    Fix: CSP for vite server

--- src/c2dl/app/Services/Csp/Policies/BasePolicy.php
+++ src/c2dl/app/Services/Csp/Policies/BasePolicy.php
@@ -4,11 +4,15 @@ namespace App\Services\Csp\Policies;
 
 use Spatie\Csp\Directive;
 use Spatie\Csp\Policies\Basic;
-use Spatie\Csp\Value;
 use Spatie\Csp\Keyword;
 
 class BasePolicy extends Basic
 {
+
+    private const STORAGE_HOST = "http://storage.c2dl.info";
+    private const LOCALHOST_DEV_VITE_HTTP = "http://localhost:5173";
+    private const LOCALHOST_DEV_VITE_WS = "ws://localhost:5173";
+
     public function configure()
     {
         if ((config('app.debug') == false) || (config('app.csp-in-debug') == true)) {
@@ -19,23 +23,37 @@ class BasePolicy extends Basic
                 ->addDirective(Directive::CONNECT, Keyword::SELF)
                 ->addDirective(Directive::DEFAULT, Keyword::SELF)
                 ->addDirective(Directive::FORM_ACTION, Keyword::SELF)
-                ->addDirective(Directive::IMG, [Keyword::SELF, "storage.c2dl.info"])
+                ->addDirective(Directive::IMG, [Keyword::SELF, BasePolicy::STORAGE_HOST])
                 ->addDirective(Directive::MEDIA, Keyword::SELF)
                 ->addDirective(Directive::OBJECT, Keyword::NONE)
                 ->addDirective(Directive::SCRIPT, Keyword::SELF)
                 ->addDirective(Directive::STYLE, [Keyword::SELF, Keyword::UNSAFE_INLINE])
-                ->addDirective(Directive::FONT, [Keyword::SELF, "storage.c2dl.info"])
+                ->addDirective(Directive::FONT, [Keyword::SELF, BasePolicy::STORAGE_HOST])
                 ->addNonceForDirective(Directive::SCRIPT);
             // ->addNonceForDirective(Directive::STYLE)
         }
         else {
             $this
-                ->addDirective(Directive::BASE, Keyword::SELF)
-                ->addDirective(Directive::DEFAULT, Keyword::SELF)
-                ->addDirective(Directive::SCRIPT, [Keyword::SELF, Keyword::UNSAFE_INLINE, "storage.c2dl.info"])
-                ->addDirective(Directive::STYLE, [Keyword::SELF, Keyword::UNSAFE_INLINE, "storage.c2dl.info"])
-                ->addDirective(Directive::IMG, [Keyword::SELF, "storage.c2dl.info"])
-                ->addDirective(Directive::FONT, [Keyword::SELF, "storage.c2dl.info"]);
+                ->addDirective(Directive::BASE,
+                    [Keyword::SELF, BasePolicy::LOCALHOST_DEV_VITE_HTTP])
+                ->addDirective(Directive::DEFAULT,
+                    [Keyword::SELF, BasePolicy::LOCALHOST_DEV_VITE_HTTP])
+                ->addDirective(Directive::SCRIPT,
+                    [
+                        Keyword::SELF, Keyword::UNSAFE_INLINE,
+                        BasePolicy::LOCALHOST_DEV_VITE_HTTP, BasePolicy::STORAGE_HOST
+                    ])
+                ->addDirective(Directive::STYLE,
+                    [
+                        Keyword::SELF, Keyword::UNSAFE_INLINE,
+                        BasePolicy::LOCALHOST_DEV_VITE_HTTP, BasePolicy::STORAGE_HOST
+                    ])
+                ->addDirective(Directive::IMG,
+                    [Keyword::SELF, BasePolicy::LOCALHOST_DEV_VITE_HTTP, BasePolicy::STORAGE_HOST])
+                ->addDirective(Directive::FONT,
+                    [Keyword::SELF, BasePolicy::LOCALHOST_DEV_VITE_HTTP, BasePolicy::STORAGE_HOST])
+                ->addDirective(Directive::CONNECT,
+                    [Keyword::SELF, BasePolicy::LOCALHOST_DEV_VITE_WS]);
         }
     }
 }
