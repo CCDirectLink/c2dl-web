a0f5049bda32676ed36d6b4de380d30f27e4b63f
streetclaw <streetclaw@c2dl.info>
2025-03-04 00:07:56 +0100

    chore: Register middleware

--- src/c2dl/bootstrap/app.php
+++ src/c2dl/bootstrap/app.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Middleware\CheckForMaintenanceMode;
+use App\Http\Middleware\EncryptCookies;
+use App\Http\Middleware\TrustProxies;
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\Middleware\StartSession;
+use Spatie\Csp\AddCspHeaders;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,8 +17,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(CheckForMaintenanceMode::class);
+        $middleware->append(StartSession::class);
+        $middleware->append(EncryptCookies::class);
+        $middleware->append(TrustProxies::class);
+        $middleware->append(VerifyCsrfToken::class);
+        $middleware->append(AddCspHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // unused
     })->create();
