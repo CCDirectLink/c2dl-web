020d8507f034915a784a1e53b42210fe222371cb
streetclaw <streetclaw@c2dl.info>
2025-03-04 21:59:17 +0100

    chore: Setup refactor

--- docker/dev_setup.sh
+++ docker/dev_setup.sh
@@ -1,53 +1,82 @@
-cd "$( dirname "$0" )" || return 1
+#!/usr/bin/env sh
 
-blank_dev_env=0
-no_db=0
-container_tool="_missing"
-
-if [ ! -d .run/mysql/c2dl ]; then
-  echo "Check for database... not found"
-  no_db=1
+# shellcheck disable=SC3054
+# shellcheck disable=SC3028
+if [ "${BASH_SOURCE[0]}" = "" ]; then
+    _SELF_DIR="$(cd -- "$(dirname -- "$0")" >/dev/null 2>&1 ||
+    exit 1; pwd -P )"
 else
-  echo "Check for database... found"
+  # shellcheck disable=SC3054
+  # shellcheck disable=SC3028
+  _SELF_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")">/dev/null 2>&1||
+  exit 1; pwd -P )"
+fi
+cd "${_SELF_DIR}" || exit 1
+
+_BLANK_DEV_ENV=0
+_NO_DB=0
+_CONTAINER_TOOL="_MISSING"
+
+_PHP_CONTAINER_NAME="c2dl-php"
+
+printf "Check for database..."
+if [ -d ".run/mysql/c2dl" ]; then
+    printf " found"
+else
+  printf " not found"
+  _NO_DB=1
+fi
+printf "\n"
+
+printf "Check for container environment..."
+if which docker; then
+    printf " Docker found"
+    _CONTAINER_TOOL="docker"
+elif which podman; then
+    printf " Podman found"
+    _CONTAINER_TOOL="podman"
+else
+    printf " No tool found\nDocker or podman missing but required\n"
+    return 1
+fi
+printf "\n"
+
+_DEV_ENV_FILE="${_SELF_DIR}/conf/env/dev_env"
+_ENV_FILE="${_SELF_DIR}/../src/c2dl/.env"
+
+if [ -f "${_ENV_FILE}" ]; then
+  printf "Copy skipped: .env file found in c2dl directory\n"
+else
+  printf "No .env file found in c2dl directory\n"
+  printf "Copy %s to %s\n" "${_DEV_ENV_FILE}" "${_ENV_FILE}"
+  cp "${_DEV_ENV_FILE}" "${_ENV_FILE}"
+  _BLANK_DEV_ENV=1
 fi
 
-if [[ $(which docker) && $(docker --version) ]]; then
-  echo "Docker found"
-  container_tool="docker"
-else
-  echo "Docker missing but required"
-  return 1
-fi
+"${_CONTAINER_TOOL}" compose up -d --wait --build
 
-if [ ! -f ../src/c2dl/.env ]; then
-  echo "No .env file found in c2dl directory - copy dev_env to c2dl/.env"
-  cp conf/env/dev_env ../src/c2dl/.env
-  blank_dev_env=1
-else
-  echo ".env file found in c2dl directory - skipped"
-fi
-
-$container_tool compose up -d --wait --build
-
-if [ $blank_dev_env -eq 1 ]; then
+if [ "${_BLANK_DEV_ENV}" -eq "1" ]; then
   echo "Create key for blank .env"
-  $container_tool exec -it c2dl-php sh -c "php artisan key:generate"
+  "${_CONTAINER_TOOL}" exec \
+  -it "${_PHP_CONTAINER_NAME}" sh -c "php artisan key:generate"
 fi
 
-if [ $no_db -eq 1 ]; then
+if [ "${_NO_DB}" -eq "1" ]; then
   dialog --stdout --title "Seed database" \
    --backtitle "Seed database" \
    --yesno "Do you want to seed the database?" 7 60
-  seed_choice=$?
+  _SEED_CHOICE=$?
   clear
 else
-  seed_choice=1
+  _SEED_CHOICE=1
 fi
 
-if [ $seed_choice -eq 0 ]; then
+if [ "${_SEED_CHOICE}" -eq "0" ]; then
   echo "Seed database"
-  $container_tool exec -it c2dl-php sh -c "php artisan migrate --seed"
+  "${_CONTAINER_TOOL}" exec \
+  -it "${_PHP_CONTAINER_NAME}" sh -c "php artisan migrate --seed"
 else
   echo "Seed database skipped"
-  $container_tool exec -it c2dl-php sh -c "php artisan migrate"
+  "${_CONTAINER_TOOL}" exec \
+  -it "${_PHP_CONTAINER_NAME}" sh -c "php artisan migrate"
 fi
