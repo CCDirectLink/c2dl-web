79509dc44d6b5db46a939a195bcf590aa3882aa5
streetclaw <streetclaw@c2dl.info>
2025-04-29 22:07:04 +0200

    feature: Use composer phar in docker environment

--- docker/dev_setup.sh
+++ docker/dev_setup.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env sh
 
-# shellcheck disable=SC3054
-# shellcheck disable=SC3028
-if [ "${BASH_SOURCE[0]}" = "" ]; then
-  _DEV_S_SELF_DIR="$(cd -- "$(dirname -- "$0")" >/dev/null 2>&1 ||
-  exit 1; pwd -P )"
-else
-  # shellcheck disable=SC3054
-  # shellcheck disable=SC3028
-  _DEV_S_SELF_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")">/dev/null 2>&1||
-  exit 1; pwd -P )"
-fi
+_DEV_S_SELF_DIR="$(cd -- "$(dirname -- "$0")" >/dev/null 2>&1 || exit 1; pwd -P )"
 cd "${_DEV_S_SELF_DIR}" || exit 1
 
 _DEV_S_DB_FOUND="false"
@@ -43,29 +33,35 @@ else
   cp "${_DEV_S_CONF_ENV_FILE}" "${_DEV_S_LARA_ENV_FILE}"
 fi
 
-"${_CONTAINER_TOOL}" compose up -d --wait --build
+"${_DEV_S_C_TOOL}" compose up -d --wait --build
 
 run_c_command()
 {
-  "${_CONTAINER_TOOL}" exec \
+  "${_DEV_S_C_TOOL}" exec \
   -it "${DEV_S_PHP_CONTAINER_NAME:-"c2dl-php"}" sh -c "${1}"
 }
+
+run_c_command "composer install"
 
 if [ "${_DEV_S_ENV_INIT}" = "false" ]; then
   run_c_command "php artisan key:generate"
 fi
 
-if [ "${_DEV_S_DB_FOUND}" = "false" ]; then
-  dialog --stdout --title "Seed database" \
-   --backtitle "Seed database" \
-   --yesno "Do you want to seed the database?" 7 60
-  _DEV_S_SEED_CHOICE=$?
-  clear
+if [ "${_DEV_S_DB_FOUND}" = "true" ]; then
+  _DEV_S_SEED_CHOICE="false"
+elif [ -z "${DEV_S_SEED_CHOICE}" ]; then
+  printf "Seed database [y/n]: "
+  read -r _DEV_S_SEED_INPUT
+  if [ "${_DEV_S_SEED_INPUT}" = "y" ]; then
+    _DEV_S_SEED_CHOICE="true"
+  else
+    _DEV_S_SEED_CHOICE="false"
+  fi
 else
-  _DEV_S_SEED_CHOICE=1
+  _DEV_S_SEED_CHOICE="$DEV_S_SEED_CHOICE"
 fi
 
-if [ "${_DEV_S_SEED_CHOICE}" -eq "0" ]; then
+if [ "${_DEV_S_SEED_CHOICE}" = "true" ]; then
   printf "Seed database\n"
   run_c_command "php artisan migrate --seed"
 else
